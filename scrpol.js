bf0513d7a8bc3f74b4c10eb01b09be8dec7dfbe9
RoahanRisqi <133971122+RoahanRisqi@users.noreply.github.com>
2025-11-19 20:36:44 +0700

    Update scrpol.js

--- scrpol.js
+++ scrpol.js
@@ -1,4 +1,8 @@
 const tombolSapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click', function(){
-  alert('Halo! Terima kasih sudah berkunjung')
-)};
+
+// Pastikan elemen ada sebelum menambahkan event listener untuk mencegah error
+if (tombolSapa) {
+    tombolSapa.addEventListener('click', function() {
+        alert('Halo! Terima kasih sudah berkunjung');
+    }); // Perbaikan urutan kurung tutup di sini
+}
